a21e00d89e34b058f06b13200257eb396fac36f4
bill-gao <bill.gao@rea-group.com>
2017-01-16 22:26:31 +1100

    Update example and docs to use MatchMediaHOC

--- examples/example.js
+++ examples/example.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MatchMedia } from '../src';
-
-const testDivStyles = {
-  backgroundColor: '#ccc',
-  color: 'red',
-  lineHeight: '100px',
-  fontSize: '50px',
-  marginTop: '50px',
-  textAlign: 'center'
-}
+import InfoPanel from './InfoPanel';
 
 ReactDOM.render(
   <div className="example">
     <div>Content below is only rendered on screen size bigger than 500px </div>
-    <MatchMedia mediaQuery={'(min-width: 500px)'}>
-      <div style={testDivStyles}>I am rendered</div>
-    </MatchMedia>
+    <InfoPanel />
   </div>,
   document.getElementById('root')
 );
